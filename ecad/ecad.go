30ad3e67e30b137608a89c7bea6601fe475cb91c
Michael Meier <distributed@gloenggi.ch>
2013-11-17 20:52:36 +0100

    add ecad: ethercat register addresses

---
+++ ecad/ecad.go
@@ -0,0 +1,34 @@
+package ecad
+
+const (
+	Type                 = 0x0000
+	Revision             = 0x0001
+	Build                = 0x0002
+	FMMUsSupported       = 0x0004
+	RAMSize              = 0x0006
+	PortDescriptor       = 0x0007
+	ESCFeaturesSupported = 0x0008
+
+	ConfiguredStationAddress = 0x0010
+	ConfiguredStationAlias   = 0x0012
+
+	DLControl = 0x0100
+	DLStatus  = 0x0110
+
+	ALControl    = 0x0120
+	ALStatus     = 0x0130
+	ALStatusCode = 0x0134
+
+	ECATEventMask = 0x0200
+
+	ESIEEPROMInterface   = 0x0500
+	EEPROMConfiguration  = 0x0500
+	EEPROMPDIAccessState = 0x0501
+	EEPROMControlStatus  = 0x0502
+	EEPROMAddress        = 0x0504
+	EEPROMData           = 0x0508
+
+	FMMUBase = 0x0600
+
+	SyncMangerBase = 0x0800
+)
