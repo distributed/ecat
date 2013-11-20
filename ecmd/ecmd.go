2641f4a60a88e242c2afaa768999f5ec8cced441
Michael Meier <distributed@gloenggi.ch>
2013-11-20 23:57:58 +0100

    add 8/16/32 bits register read/write functions

--- ecmd/ecmd.go
+++ ecmd/ecmd.go
@@ -89,6 +89,48 @@ func (o Options) getFramelossTries() int {
 }
 func (o Options) getWCDeadline() time.Time { return o.WCDeadline }
 
+func ExecuteRead8(c Commander, addr ecfr.DatagramAddress, expwc uint16) (d uint8, err error) {
+	return ExecuteRead8Options(c, addr, expwc, Options{})
+}
+
+func ExecuteRead8Options(c Commander, addr ecfr.DatagramAddress, expwc uint16, opts Options) (d uint8, err error) {
+	var ds []byte
+	ds, err = ExecuteRead(c, addr, 1, expwc)
+	if err != nil {
+		return
+	}
+	d = xgetUint8(ds)
+	return
+}
+
+func ExecuteRead16(c Commander, addr ecfr.DatagramAddress, expwc uint16) (d uint16, err error) {
+	return ExecuteRead16Options(c, addr, expwc, Options{})
+}
+
+func ExecuteRead16Options(c Commander, addr ecfr.DatagramAddress, expwc uint16, opt Options) (d uint16, err error) {
+	var ds []byte
+	ds, err = ExecuteRead(c, addr, 2, expwc)
+	if err != nil {
+		return
+	}
+	d = xgetUint16(ds)
+	return
+}
+
+func ExecuteRead32(c Commander, addr ecfr.DatagramAddress, expwc uint16) (d uint32, err error) {
+	return ExecuteRead32Options(c, addr, expwc, Options{})
+}
+
+func ExecuteRead32Options(c Commander, addr ecfr.DatagramAddress, expwc uint16, opt Options) (d uint32, err error) {
+	var ds []byte
+	ds, err = ExecuteRead(c, addr, 4, expwc)
+	if err != nil {
+		return
+	}
+	d = xgetUint32(ds)
+	return
+}
+
 func ExecuteRead(c Commander, addr ecfr.DatagramAddress, n int, expwc uint16) (d []byte, err error) {
 	return ExecuteReadOptions(c, addr, n, expwc, Options{})
 }
@@ -153,6 +195,36 @@ func ExecuteReadOptions(c Commander, addr ecfr.DatagramAddress, n int, expwc uin
 	}
 
 	panic("not reached")
+}
+
+func ExecuteWrite8(c Commander, addr ecfr.DatagramAddress, w uint8, expwc uint16) (err error) {
+	return ExecuteWrite8Options(c, addr, w, expwc, Options{})
+}
+
+func ExecuteWrite8Options(c Commander, addr ecfr.DatagramAddress, w uint8, expwc uint16, opts Options) (err error) {
+	ws := make([]byte, 1)
+	putUint8(ws, w)
+	return ExecuteWriteOptions(c, addr, ws, expwc, opts)
+}
+
+func ExecuteWrite16(c Commander, addr ecfr.DatagramAddress, w uint16, expwc uint16) (err error) {
+	return ExecuteWrite16Options(c, addr, w, expwc, Options{})
+}
+
+func ExecuteWrite16Options(c Commander, addr ecfr.DatagramAddress, w uint16, expwc uint16, opts Options) (err error) {
+	ws := make([]byte, 2)
+	putUint16(ws, w)
+	return ExecuteWriteOptions(c, addr, ws, expwc, opts)
+}
+
+func ExecuteWrite32(c Commander, addr ecfr.DatagramAddress, w uint32, expwc uint16) (err error) {
+	return ExecuteWrite32Options(c, addr, w, expwc, Options{})
+}
+
+func ExecuteWrite32Options(c Commander, addr ecfr.DatagramAddress, w uint32, expwc uint16, opts Options) (err error) {
+	ws := make([]byte, 4)
+	putUint32(ws, w)
+	return ExecuteWriteOptions(c, addr, ws, expwc, opts)
 }
 
 func ExecuteWrite(c Commander, addr ecfr.DatagramAddress, w []byte, expwc uint16) (err error) {
---
+++ ecmd/marshalling.go
@@ -0,0 +1,50 @@
+package ecmd
+
+// the "native" byte ordering is the little endian encoding scheme
+// of ehthercat. big endian routines for the encoding used by ethernet
+// are provided below
+
+func getUint8(b []byte) (uint8, []byte) {
+	return b[0], b[1:]
+}
+
+func getUint16(b []byte) (uint16, []byte) {
+	return uint16(b[0]) | uint16(b[1])<<8, b[2:]
+}
+
+func getUint32(b []byte) (uint32, []byte) {
+	v := uint32(b[0]) | uint32(b[1])<<8 | uint32(b[2])<<16 | uint32(b[3])<<24
+	return v, b[4:]
+}
+
+func xgetUint8(b []byte) uint8 {
+	return b[0]
+}
+
+func xgetUint16(b []byte) uint16 {
+	return uint16(b[0]) | uint16(b[1])<<8
+}
+
+func xgetUint32(b []byte) uint32 {
+	v := uint32(b[0]) | uint32(b[1])<<8 | uint32(b[2])<<16 | uint32(b[3])<<24
+	return v
+}
+
+func putUint8(b []byte, v uint8) []byte {
+	b[0] = v
+	return b[1:]
+}
+
+func putUint16(b []byte, v uint16) []byte {
+	b[0] = uint8(v)
+	b[1] = uint8(v >> 8)
+	return b[2:]
+}
+
+func putUint32(b []byte, v uint32) []byte {
+	b[0] = uint8(v)
+	b[1] = uint8(v >> 8)
+	b[2] = uint8(v >> 16)
+	b[3] = uint8(v >> 24)
+	return b[4:]
+}
